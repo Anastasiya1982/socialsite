5ea551b8505d09359f6333b51d9341d357b10ad9
ACER <mahanasty@mail.ru>
2020-04-16 00:34:28 +0300

    props stait in Profile

--- src/index.js
+++ src/index.js
@@ -3,31 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
- let posts =[
-     {id:1, message:"Hi! How are you?",like:5},
-     {id:2, message:"It's my first post", like:12},
- ];
- let dialogs=[
-     {id:1,name:"Dimych"},
-     {id:2,name:"Andrey"},
-     {id:3,name:"Viktor"},
-     {id:4,name:"Sasha"},
-     {id:5,name:"Valera"},
-     {id:6,name:"Ihor"}
- ];
- let messages =[
-     {id:1, message:"Hi"},
-     {id:2, message:"How are you?"},
-     {id:3, message:"I want to meet with you?"},
-     {id:4, message:"Lets go for a walk"}
- ];
+import state from "./redux/state";
 
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App posts={posts} dialogs={dialogs} messages={messages}/>
+    <App state={state}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/redux/state.js
@@ -0,0 +1,29 @@
+
+let state = {
+    profilePage: {
+        posts: [
+            {id: 1, message: "Hi! How are you?", like: 5},
+            {id: 2, message: "It's my first post", like: 12},
+        ],
+
+    },
+    dialogsPage: {
+        dialogs: [
+            {id: 1, name: "Dimych"},
+            {id: 2, name: "Andrey"},
+            {id: 3, name: "Viktor"},
+            {id: 4, name: "Sasha"},
+            {id: 5, name: "Valera"},
+            {id: 6, name: "Ihor"}
+        ],
+        messages: [
+            {id: 1, message: "Hi"},
+            {id: 2, message: "How are you?"},
+            {id: 3, message: "I want to meet with you?"},
+            {id: 4, message: "Lets go for a walk"}
+        ]
+    }
+
+}
+
+export default state;
