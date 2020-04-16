6cffc36dda33eb8cf304f1085ecee00b9a3be884
ACER <mahanasty@mail.ru>
2020-04-17 00:10:56 +0300

    add callback in MyPost and Message

--- src/components/Dialogs/Message/Message.jsx
+++ src/components/Dialogs/Message/Message.jsx
@@ -1,8 +1,27 @@
 import React from 'react';
 import s from './Message.module.css';
 
-const Message=(props)=>{
-    return(
-        <div className={s.dialog}>{props.message }</div>)
+const Message = (props) => {
+
+    let newMessageElement = React.createRef();
+
+    let addMessage = ()=>{
+        let text = newMessageElement.current.value;
+        alert(text);
+    }
+
+    return (
+        <div>
+            <div>
+                <div>
+                    <textarea  ref={newMessageElement}></textarea>
+                </div>
+                <div>
+                    <button onClick={addMessage}>Add post</button>
+                </div>
+            </div>
+            <div className={s.dialog}>{props.message}</div>
+        </div>
+    )
 }
 export default Message;
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,16 +3,25 @@ import  s from './MyPosts.module.css';
 import Post from "./Post/Post";
 
 const MyPosts = (props) =>{
-    let postsElement= props.posts.map(p=>(<Post massage={p.message} like={p.like}/>));
+    let postsElement=
+        props.posts.map(p=>(<Post massage={p.message} like={p.like}/>));
+
+    let newPostElement = React.createRef();
+
+    let addPost =()=>{
+       let text=newPostElement.current.value;
+       alert(text);
+   }
+
     return  (
         <div className={s.postwrapp}>
             <h3>My posts</h3>
             <div>
                 <div>
-                    <textarea></textarea>
+                    <textarea ref = { newPostElement}></textarea>
                 </div>
                 <div>
-                    <button>Add post</button>
+                    <button onClick={addPost}>Add post</button>
                 </div>
                 <div className={s.posts}>
                     {postsElement}
