897e4c34ace369d3d0ff4b7e2cdce0a5b6788aea
ACER <mahanasty@mail.ru>
2020-06-03 13:38:48 +0300

    refactoring users

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -3,83 +3,57 @@ import s from './Users.module.css';
 import * as axios from 'axios';
 import userPhoto from '../../assets/img/man.png';
 
-let Users=(props)=> {
-    if (props.users.length === 0) {
-        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-            props.setUsers(response.data.items);
 
-        });
+ class Users  extends React.Component{
+     constructor(props) {
+         super(props);
+     }
+     componentDidMount() {
+         axios.get("https://social-network.samuraijs.com/api/1.0/users")
+             .then(response => {
+                 this.props.setUsers(response.data.items);
+             });
+     }
 
-    }
+     render(){
+         return <div>
+             {
+                this.props.users.map(u => <div key={u.id}>
+                         <div className={s.userItem}>
+                             <div className={s.usersImg}>
+                                 <div>
+                                     <img className={s.usersPhoto}
+                                          alt="logo-Photo"
+                                          src={u.photos.small != null ? u.photos.small : userPhoto}/>                                } />
+                                 </div>
+                                 <div>
+                                     {u.followed
+                                         ? <button onClick={() => {
+                                             this.props.unfollow(u.id)
+                                         }}>UnFollow</button>
+                                         : <button onClick={() => {
+                                             this.props.follow(u.id)
+                                         }}>Follow</button>
+                                     }
 
-        // props.setUsers([
-        //         {
-        //             id: 1, photoUrl: 'https://sun9-28.userapi.com/c637625/v637625419/b95a/3mIndGkkUTs.jpg',
-        //             followed: true, fullName: "Katya", status: "friend", location:
-        //                 {city: "Minsk", country: "Belarus"}
-        //         },
-        //         {
-        //             id: 2, photoUrl: "https://pp.userapi.com/c638430/v638430295/32c31/C4J0O_w4ewk.jpg",
-        //             followed: true, fullName: "Dmitriy", status: "friend", location:
-        //                 {city: "Vitebsk", country: "Belarus"}
-        //         },
-        //         {
-        //             id: 3, photoUrl: 'https://sun9-28.userapi.com/c637625/v637625419/b95a/3mIndGkkUTs.jpg',
-        //             followed: false, fullName: "Svetlana", status: "new", location:
-        //                 {city: "Varshava", country: "Poland"}
-        //         },
-        //         {
-        //             id: 4,
-        //             photoUrl: 'https://avatars.mds.yandex.net/get-pdb/195449/6eec6f67-e740-457e-9eb7-34bcde2f7082/s1200?webp=false',
-        //             followed: false,
-        //             fullName: "Olga",
-        //             status: "new",
-        //             location:
-        //                 {city: "Moskow", country: "Russia"}
-        //         },
-        //
-        //     ]
-        //)
+                                 </div>
+                             </div>
+                             <div className={s.userInfo}>
 
-
-
-    return <div>
-        {
-            props.users.map(u => <div key={u.id}>
-                    <div className={s.userItem}>
-                        <div className={s.usersImg}>
-                            <div>
-                                <img className={s.usersPhoto}
-                                     alt="logo-Photo"
-                                     src={u.photos.small != null ? u.photos.small : userPhoto}/>                                } />
-                            </div>
-                            <div>
-                                {u.followed
-                                    ? <button onClick={() => {
-                                        props.unfollow(u.id)
-                                    }}>UnFollow</button>
-                                    : <button onClick={() => {
-                                        props.follow(u.id)
-                                    }}>Follow</button>
-                                }
-
-                            </div>
-                        </div>
-                        <div className={s.userInfo}>
-
-                            <div>
-                                <span>{u.name}</span>
-                                <span>{u.status}</span>
-                            </div>
-                            <div>
-                                <span>{"u.location.country"}</span>
-                                <span>{"u.location.city"}</span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            )
-    }
-    </div>
-}
+                                 <div>
+                                     <span>{u.name}</span>
+                                     <span>{u.status}</span>
+                                 </div>
+                                 <div>
+                                     <span>{"u.location.country"}</span>
+                                     <span>{"u.location.city"}</span>
+                                 </div>
+                             </div>
+                         </div>
+                     </div>
+                 )
+             }
+         </div>
+     }
+ }
 export default Users;
