8e7e8ef8d995c6a37b2d4c5031f0310bd081931a
ACER <mahanasty@mail.ru>
2020-06-09 11:20:02 +0300

    regitration my login'

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import s from './Header.module.css';
+import {NavLink} from "react-router-dom";
 
-const Header = () =>{
+const Header = (props) =>{
     return  <header className ={s.header}>
         <img src = "https://image.freepik.com/free-vector/eye-camera-logo_53295-268.jpg"/>
+       <div className={s.loginBlock}>
+           { props.isAuth? props.login
+           : <NavLink to={'/login'} >Login</NavLink>}
+      </div>
     </header>
 }
 
