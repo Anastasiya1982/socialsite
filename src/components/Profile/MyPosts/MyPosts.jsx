e268e3b4f74f7abae795264489cfbae5b48de188
ACER <mahanasty@mail.ru>
2020-07-12 11:50:29 +0300

    добавление к компоненте  memo

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { PureComponent} from 'react';
 import  s from './MyPosts.module.css';
 import Post from "./Post/Post";
 import {Field, reduxForm} from "redux-form";
@@ -7,26 +7,28 @@ import {Textarea} from "../../common/FormControls/FormsControls";
 
 let  maxLength10= maxLengthCreator(10);
 
-const MyPosts = (props) =>{
-     let postsElement=
-        props.posts.map(p=>(<Post massage={p.message} like={p.like}/>));
+const  MyPosts =React.memo((props) => {
+        let postsElement =
+            [...props.posts]
+                .reverse()
+                .map(p => (<Post massage={p.message} like={p.like}/>));
 
-    let onAddPost =(values)=>{
-        props.addPost(values.newMyPostText);
-   }
+        let onAddPost = (values) => {
+            props.addPost(values.newMyPostText);
+        }
 
-    return  (
-        <div className={s.postwrapp}>
-            <h3>My posts</h3>
-            <div>
-               <AddNewMyPostFormRedux onSubmit={onAddPost} />
-                <div className={s.posts}>
-                    {postsElement}
+        return (
+            <div className={s.postwrapp}>
+                <h3>My posts</h3>
+                <div>
+                    <AddNewMyPostFormRedux onSubmit={onAddPost}/>
+                    <div className={s.posts}>
+                        {postsElement}
+                    </div>
                 </div>
             </div>
-        </div>
-    )
-}
+        )
+    });
 
 const AddMyPostForm=(props)=>{
     return(
