27f7503e0f2b70f3039657d1192a6c7e293b783e
ACER <mahanasty@mail.ru>
2020-07-17 22:31:34 +0300

    Updates

---
+++ precache-manifest.0daaa569bfb2b8ead765a6605e483378.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "507d628b5b3d5c96a8f79de7f87f0657",
+    "url": "/index.html"
+  },
+  {
+    "revision": "47ee34a2b3ec507167ba",
+    "url": "/static/css/3.58d74acf.chunk.css"
+  },
+  {
+    "revision": "560d45999f4cc903e267",
+    "url": "/static/css/main.ecc2df84.chunk.css"
+  },
+  {
+    "revision": "14babebc67d676285f59",
+    "url": "/static/js/2.0730570c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.0730570c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47ee34a2b3ec507167ba",
+    "url": "/static/js/3.96d91fe8.chunk.js"
+  },
+  {
+    "revision": "560d45999f4cc903e267",
+    "url": "/static/js/main.80df23f1.chunk.js"
+  },
+  {
+    "revision": "17800246e2d53ca4c9bb",
+    "url": "/static/js/runtime-main.065f17cb.js"
+  },
+  {
+    "revision": "b6ff8505492c2697fe1641153606f45d",
+    "url": "/static/media/loading.b6ff8505.gif"
+  },
+  {
+    "revision": "73fb0d96b75a0d94c73ee0c1a8b44826",
+    "url": "/static/media/man.73fb0d96.png"
+  }
+]);
