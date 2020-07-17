22df533784d0cbb223192c242e04b281b31e9c6d
ACER <mahanasty@mail.ru>
2020-07-17 22:33:54 +0300

    Updates

---
+++ precache-manifest.7073b54c35300b2b517bbce54136b75f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5162f2146863d7df414cae20779d3a9",
+    "url": "/socialsite/index.html"
+  },
+  {
+    "revision": "d736188474aac2cf36e8",
+    "url": "/socialsite/static/css/3.58d74acf.chunk.css"
+  },
+  {
+    "revision": "50720db436f7a5697225",
+    "url": "/socialsite/static/css/main.ecc2df84.chunk.css"
+  },
+  {
+    "revision": "a6b02f3e02498361d964",
+    "url": "/socialsite/static/js/2.58dfc364.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/socialsite/static/js/2.58dfc364.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d736188474aac2cf36e8",
+    "url": "/socialsite/static/js/3.848c8bd4.chunk.js"
+  },
+  {
+    "revision": "50720db436f7a5697225",
+    "url": "/socialsite/static/js/main.5c201649.chunk.js"
+  },
+  {
+    "revision": "3c4d64f42b2ed6c9fcb9",
+    "url": "/socialsite/static/js/runtime-main.b92726d6.js"
+  },
+  {
+    "revision": "b6ff8505492c2697fe1641153606f45d",
+    "url": "/socialsite/static/media/loading.b6ff8505.gif"
+  },
+  {
+    "revision": "73fb0d96b75a0d94c73ee0c1a8b44826",
+    "url": "/socialsite/static/media/man.73fb0d96.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0daaa569bfb2b8ead765a6605e483378.js"
+  "/socialsite/precache-manifest.7073b54c35300b2b517bbce54136b75f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/socialsite/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
